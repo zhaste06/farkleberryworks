56f077f0175cb5a3cfdac67fce0c57a018350acd
Lance Lentz <lancelentz@live.com>
2018-11-12 14:14:05 -0800

    file fixing

--- cis4270/guitarShop/views/productView.php
+++
@@ -1,9 +0,0 @@
-<?php require('views/guitarShopHeader.php'); ?>
-<main>
-<section>
-    <!-- display product -->
-    <?php require (APP_NON_WEB_BASE_DIR .'views/product.php'); ?>
-</section>
-</main>
-<?php
-require('views/guitarShopFooter.php');
