45c94662777c16236b915ad2af85c01d809a5775
zHaste06 <zhaste06@yahoo.com>
2018-11-24 11:44:31 -0800

    Update loginState.php

--- guitarShop/includes/loginState.php
+++ guitarShop/includes/loginState.php
@@ -5,6 +5,7 @@ if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==1 && time() - $_SESS
     $_SESSION['time'] = time(); //update session time to current time as Unix timestamp of integer type
 }
 else {
+    $sessionTimeOutMessage = 'SESSION HAS TIMED OUT. Please Log In.'; //creates a message to be displayed in homepage
     session_unset(); //unset all $_SESSION variables
     session_destroy(); //destroy session data
     header("/index.php"); //redirect to home
