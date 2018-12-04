1b1286eb6fd179303f141a65236864840f14ab10
Lance <lancelentz@live.com>
2018-12-04 07:48:18 -0800

    other vm testing

--- guitarShop/controllers/DefaultController.php
+++ guitarShop/controllers/DefaultController.php
@@ -38,9 +38,9 @@ class DefaultController {
             $vm = ProductsVM::getFeaturedInstance();
         }
         if (!isset($_SESSION)) {session_start();}
-        $vm = AccountVM::accountInstance();
-        if (isset($vm->User)) {
-          echo "Welcome " . $vm->User->firstname;
+        $vmAcc = AccountVM::accountInstance();
+        if (isset($vmAcc->User)) {
+          echo "Welcome " . $vmAcc->User->firstname;
         }
         // Go to the default view of the application.
         require(APP_NON_WEB_BASE_DIR . 'views/home.php');
